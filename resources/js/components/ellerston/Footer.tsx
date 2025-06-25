74fe825becbdcd144fa3a2ab7c47e00c865fb0e0
anil-thepistol <anil.kumar@thepistol.com.au>
2025-06-25 17:51:24 +0530

    script changes for animation

--- resources/js/components/ellerston/Footer.tsx
+++ resources/js/components/ellerston/Footer.tsx
@@ -1,21 +1,23 @@
 import { Link } from '@inertiajs/react';
 import { gsap } from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import { useEffect } from 'react';
 gsap.registerPlugin(ScrollTrigger);
-window.addEventListener('load', () => {
-    gsap.fromTo(
-        '.foot_link',
-        { opacity: 0, y: 80 },
-        {
-            opacity: 1,
-            y: 0,
-            duration: 0.5,
-            stagger: 0.3,
-            delay: 1,
-        },
-    );
-});
+
 function Footer() {
+    useEffect(() => {
+        gsap.fromTo(
+            '.foot_link',
+            { opacity: 0, y: 80 },
+            {
+                opacity: 1,
+                y: 0,
+                duration: 0.5,
+                stagger: 0.3,
+                delay: 1,
+            },
+        );
+    }, []);
     return (
         <footer className="footer absolute right-0 bottom-[40px] left-0 z-10">
             <div className="mx-auto flex w-[868px] text-center text-[14px] leading-[24px] tracking-[12%] text-white uppercase">
