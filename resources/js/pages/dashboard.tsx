b81553ae5c2df484fecccf1ca7de01657007d912
Nitish <nk3227610@gmail.com>
2025-07-21 19:57:35 +0530

    small points

--- resources/js/pages/dashboard.tsx
+++ resources/js/pages/dashboard.tsx
@@ -18,9 +18,13 @@ export default function Dashboard() {
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
 
 
+                {/* changed by Nitish */}
+
                 <div className="">
                     <Link href="/upload-csv" className='hover:text-blue-400 py-2 px-4 border border-gray-200 rounded-lg'>Upload CSV</Link>
                 </div>
+
+                {/* till here */}
 
                 <div className="grid auto-rows-min gap-4 md:grid-cols-3">
                     <div className="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
